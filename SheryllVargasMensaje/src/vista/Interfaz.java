2a2f033a7d13230fd21021ddd8f4615292824486
Sheryll-Vargas19 <sheryll_vargas@hotmail.com>
2019-10-18 06:34:03 -0500

    Parcial Piratas

---
+++ SheryllVargasMensaje/src/vista/Interfaz.java
@@ -0,0 +1,17 @@
+package vista;
+
+import javax.swing.JOptionPane;
+
+public class Interfaz {
+	
+	public String escribirDato(String recibirLlaves) {
+		
+		JOptionPane.showMessageDialog(null,recibirLlaves);
+		return null;
+		
+	}
+
+	public Interfaz() {
+		
+	}
+}
